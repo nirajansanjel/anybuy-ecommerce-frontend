e62afd8e80e2cd8070f478496bf20895a2cc571b
nirajansanjel <sanjelnirajan13@gmail.com>
2026-01-18 04:08:02 +0545

    feat:interceptors setup

--- src/api/products.js
+++ src/api/products.js
@@ -1,9 +1,15 @@
 import config from "@/config";
 import axios from "axios";
+import api from "./api";
 async function getProducts() {
   return await axios.get(`${config.apiUrl}/api/products`);
 }
 async function getProductById(id) {
   return await axios.get(`${config.apiUrl}/api/products/${id}`);
 }
-export { getProducts, getProductById };
+
+async function createProduct(data, authToken) {
+  return await api.post(`/api/products/create`, data);
+}
+
+export { getProducts, getProductById, createProduct };
