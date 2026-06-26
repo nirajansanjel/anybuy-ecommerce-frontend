756f4065717093dc735fe90bd82bec79568934ce
nirajansanjel <sanjelnirajan13@gmail.com>
2026-06-26 22:40:18 +0545

    feat: cart route fix

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,21 +1,17 @@
-export  const HOME_ROUTE = "/"
-export  const ABOUT_ROUTE = "/about"
-export  const POPULAR_ROUTE = "/popular"
-export  const PRODUCTS_ROUTE = "/products"
-export  const ORDERS_ROUTE = "/orders"
-export  const PRODUCTS_CART_ROUTE = "products/cart"
-export  const CATEGORIES_ROUTE = "/categories"
-export  const TESTIMONIALS_ROUTE = "/testimonials"
-export  const LOGIN_ROUTE = "/login"
-export  const PRODUCT_MANAGEMENT_ROUTE = "/product-management"
-export  const ORDER_MANAGEMENT_ROUTE = "/order-management"
-export  const USER_MANAGEMENT_ROUTE = "/user-management"
-export  const DASHBOARD_ROUTE = "/dashboard"
-export  const PROFILE_ROUTE = "/profile"
-export  const PRODUCTS_ORDERS_ROUTE = "/orders"
-export  const FORGOT_PASSWORD_ROUTE = "/forgot-password"
-export  const RESET_PASSWORD_ROUTE = "/reset-password"
-
-
-
-
+export const HOME_ROUTE = "/";
+export const ABOUT_ROUTE = "/about";
+export const POPULAR_ROUTE = "/popular";
+export const PRODUCTS_ROUTE = "/products";
+export const ORDERS_ROUTE = "/orders";
+export const PRODUCTS_CART_ROUTE = "/products/cart";
+export const CATEGORIES_ROUTE = "/categories";
+export const TESTIMONIALS_ROUTE = "/testimonials";
+export const LOGIN_ROUTE = "/login";
+export const PRODUCT_MANAGEMENT_ROUTE = "/product-management";
+export const ORDER_MANAGEMENT_ROUTE = "/order-management";
+export const USER_MANAGEMENT_ROUTE = "/user-management";
+export const DASHBOARD_ROUTE = "/dashboard";
+export const PROFILE_ROUTE = "/profile";
+export const PRODUCTS_ORDERS_ROUTE = "/orders";
+export const FORGOT_PASSWORD_ROUTE = "/forgot-password";
+export const RESET_PASSWORD_ROUTE = "/reset-password";
